31a8d2bc0004f6859614b812ed6bbfc95b397c0c
Carlos Eduardo Ferraz Augusto <cadu.augusto10@gmail.com>
2025-03-25 14:13:08 -0300

    feat: create page with View, Text and Image

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-//import Stack1 from './navigation/DrawerNavigator';
-import Stack2 from './navigation/StackNavigator';
-//import Stack3 from './navigation/TabNavigator';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack2 />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.header}>Mago Elétrico</Text>
+      <Image source={require("./assets/image.jpg")} style={styles.image}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+
+  },
+  image: {
+    width: 700,
+    height: 500, 
+    margin: 20,
+  },
+  header:{
+    display: 'flex',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    backgroundColor: 'lightblue',
+    width: '100%',
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
